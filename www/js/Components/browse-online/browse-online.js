17bcc684efe02af525134ae599167527b390ecec
Tyeren <dirt3199@yahoo.com>
2016-09-27 09:38:19 -0600

    Created next page and previous page button on online view to switch between online recipe pages

--- www/js/Components/browse-online/browse-online.js
+++ www/js/Components/browse-online/browse-online.js
@@ -12,9 +12,26 @@
       vm.$onInit = onInit;
       vm.onlineDetails = onlineDetails;
       vm.goBack = goBack;
+      vm.nextPage = nextPage;
+      vm.previousPage = previousPage;
+      var pageNumber = 1;
 
       function goBack() {
         vm.selectedRecipe = undefined;
+      }
+
+      function nextPage() {
+        pageNumber = pageNumber + 1;
+        vm.recipes = [];
+        apiCall(pageNumber);
+      }
+
+      function previousPage() {
+        if (pageNumber > 1) {
+          pageNumber = pageNumber - 1;
+          vm.recipes = [];
+        }
+        apiCall(pageNumber);
       }
 
       function onlineDetails(recipeId) {
@@ -28,15 +45,19 @@
         })
       }
 
-      function onInit() {
-        apiCallsService.browseRecipes().then(
+      function apiCall(pageNumber) {
+        apiCallsService.browseRecipes(pageNumber).then(
           function (sucessResponse) {
             vm.recipes = sucessResponse;
             console.log(vm.recipes);
           }, function (errorResponse) {
 
-        }
+          }
         )
+      }
+
+      function onInit() {
+        apiCall(pageNumber);
       }
 
     }
