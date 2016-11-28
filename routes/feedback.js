15281901961b13a7cb941448b0f63acb98c5973b
loganhendricks <lhendricks88@gmail.com>
2016-11-28 09:54:35 -0700

    updated the feedback creation to work off of mongo

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var Feedback = require('../models/models.js').Feedback;
 
 router.post('/create', function (req, res, next) {
-
+  Feedback.create(req.body, function (err, feedback) {
+    if(err) {
+      res.status(404).send({ error: err });
+    }
+    else {
+      res.json(feedback);
+    }
+  });
 });
 
