340f91be9458f15a91bc32f5137853f666da131b
loganhendricks <lhendricks88@gmail.com>
2016-12-05 09:54:33 -0700

    starting passport update on logan's branch becasuse I accidenlty had it get into mobile branch 2

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/models.js').User;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../models/models.js').User;
 
 
 passport.use(new FacebookStrategy({
